7fd747ff5385e7c791cc14a55d5e7caebbdf2b5a
coffeemakingtoaster <coffeemakingtoaster@gmail.com>
2025-05-30 12:24:47 +0200

    feat(python in go): add test

---
+++ python-in-go/go.mod
@@ -0,0 +1,12 @@
+module github.com/coffeemakingtoaster/fiddle/python-in-go
+
+go 1.23.0
+
+require (
+	github.com/gofrs/flock v0.12.1 // indirect
+	github.com/kluctl/go-embed-python v0.0.0-3.13.1-20241219-1 // indirect
+	github.com/sirupsen/logrus v1.9.3 // indirect
+	golang.org/x/sync v0.10.0 // indirect
+	golang.org/x/sys v0.28.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
---
+++ python-in-go/main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/kluctl/go-embed-python/python"
+	"gopkg.in/yaml.v3"
+)
+
+type Sets struct {
+	Sets []Set `yaml:"sets"`
+}
+
+type Set struct {
+	Name string `yaml:"name"`
+	Code string `yaml:"code"`
+}
+
+func run(code string) {
+	ep, err := python.NewEmbeddedPython("example")
+	if err != nil {
+		panic(err)
+	}
+
+	cmd, err := ep.PythonCmd("-c", code)
+	if err != nil {
+		panic(err)
+	}
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	err = cmd.Run()
+	if err != nil {
+		panic(err)
+	}
+}
+
+func main() {
+
+	fmt.Println("Starting...")
+
+	yamlFile, err := os.ReadFile("./test.yaml")
+	if err != nil {
+		panic(err)
+	}
+
+	var sets Sets
+
+	err = yaml.Unmarshal(yamlFile, &sets)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Loaded %d sets\n", len(sets.Sets))
+
+	for _, v := range sets.Sets {
+		fmt.Printf("Name: %s\n", v.Name)
+		run(v.Code)
+	}
+}
